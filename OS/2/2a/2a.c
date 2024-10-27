ccf05ba9eb58219d28ac483c89bc09db116a2fbb
ParasNingune <153178176+ParasNingune@users.noreply.github.com>
2024-10-27 22:00:47 +0530

    Simplified 2a.c

--- OS/2/2a/2a.c
+++ OS/2/2a/2a.c
@@ -1,94 +1,98 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/types.h>
-#define MAX 20
-void bubble_asc(int arr[],int n)
-{ 
-int i,j,temp;
-for(i=0;i<n;i++)
-{
-    for(j=0;j<n-i-1;j++)
-    {
-        if(arr[j]>arr[j+1])
-	{
-	    temp=arr[j];
-	    arr[j]=arr[j+1];
-	    arr[j+1]=temp;
-	}
-    }
-}
-}
-void bubble_dsc(int arr[],int n)
-{ 
-int i,j,temp;
-for(i=0;i<n;i++)
-{
-    for(j=0;j<n-i-1;j++)
-    {
-        if(arr[j]<arr[j+1])
-	{
-	    temp=arr[j];
-	    arr[j]=arr[j+1];
-	    arr[j+1]=temp;
-	}
-    }
-}
-}
-void print(int arr[],int n)
-{
-   int i;
-   for(i=0;i<n;i++)
-   {
-       printf("\t%d",arr[i]);
-   }
-   printf("\n\n");
-}
-int main()
-{
-   int i,n,arr[MAX],f,pid;
-   printf("\nHOW MANY NOS DO YOU WANT IN ARRAY : ");
-   scanf("%d",&n);
-   printf("ENTER ARRAY ELEMENT : ");
-   for(i=0;i<n;i++)
-   {
-       scanf("%d",&arr[i]);
-   }
-   pid=fork();
+#include<stdlib.h>
 
-   if(pid==0)
-   {
-         /*ORPHAN STATE */
-        printf("\n\t\t**********ORPHAN STATE**************\n");
-        sleep(5);
-        printf("\n\t CHILD PROCESS PID : %d ", getpid());
-        printf("\n\t PARENT PROCESS PPID : %d",getppid());
-        system("ps -el | grep init");
-   
-        printf("\n\nARRAY IN ASSCENDING ORDER : ");
-	    bubble_asc(arr,n);
-	    print(arr,n);
-	
-   }
-   
-   else if(pid >0)
-   {
-           /*ZOMBIE STATE */
-        
-        
-        printf("\n\t\t\t**********ZOMBIE STATE**************\n");
-        system("ps -el | grep Z");
-        f=wait(0);
-       
-        printf("\n\tTERMINATED CHILD PID : %d",f);
-        printf("\n\tPARENT PID (MAIN) : %d",getpid());
-        printf("\n\tPARENTS PARENT PPID (BASH) : %d ",getppid());
-    	printf("\n\nARRAY IN DESCENDING ORDER : ");
-        bubble_dsc(arr,n);
-        print(arr,n);
-        
-   }
-   else
-      printf("\nERROR IN FORK!!");
-      
-   return 0;
+void print(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        printf("\t%d",arr[i]);
+    }
+    printf("\n\n");
+}
+
+void bubble_asc(int arr[], int n)
+{
+    int temp;
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void bubble_dec(int arr[], int n)
+{
+    int temp;
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+        {
+            if(arr[j]<arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() 
+{
+    printf("\n\n");
+    int arr[10],n,i;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("Enter no: ");
+        scanf("%d", &arr[i]);
+    }
+
+    int pid = fork();
+
+    if(pid == 0)
+    {
+        // Orphan State
+        printf("\n*******************************ORPHAN STATE*******************************\n");
+
+        printf("\nChild Process PID: %d", getpid());
+        printf("\nParent Process PPID: %d", getppid());
+
+        printf("\n\nArray in Ascending Order......\n");
+        bubble_asc(arr, n);
+        print(arr, n);
+    }
+
+    else if (pid > 0)
+    {
+        // Zombie State
+        printf("\n*******************************ZOMBIE STATE*******************************\n");
+        int tid = wait(0);
+
+        printf("\nTerminated Child PID: %d", tid);
+        printf("\nParent Process PID: %d", getpid());
+        printf("\nParent's Parent Process PPID (Bash): %d", getppid());
+
+        printf("\n\nArray in Descending Order......\n");
+        bubble_dec(arr, n);
+        print(arr, n);
+    }
+
+    else
+    {
+        printf("\nError forking process...");
+    }
+
+    return 0;
 }
